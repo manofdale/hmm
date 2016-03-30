0c81fabcbf31eed066b1594d0c41ecf0e71e25fe
AYDIN GOZE POLAT <agplt@hotmail.com>
2016-03-31 02:57:19 +0300

    viterbi

--- ds/pgm.py
+++ ds/pgm.py
@@ -1,9 +1,44 @@
+import bisect
+import numpy as np
+class MarkovState(object):
+    def __init__(self, state_id, parents=None, children=None, direction=0):
+        self.id = state_id
+        if direction >= 0:  # only store children -> e.g. for simulation
+            self.children = children
+        if direction <= 0:  # only store parents -> e.g. for viterbi
+            self.parents = parents
+
 
 class HiddenMarkov(object):
-    def __init__(self, initial_prob=None, transition_prob=None, emission_prop=None, end_state='E', start_state='B',log_prob=True):
+    def __init__(self, direction=0, initial_prob=None, transition_prob=None, emission_prop=None, end_state='E',
+                 start_state='B', log_prob=True):
         pass
 
+    def is_valid(self):
+        return (self.emitters is not None and len(self.emitters) > 0 and self.starting_p is not None and len(
+            self.starting_p) > 0
+    def random_pick(self, p_ranges):
+        p_sum=p_ranges[0]
+        for i in range(1,len(p_ranges)):
+            p_sum+=p_ranges[i]
+            p_ranges[i]=p_sum
+        return bisect.bisect(p_ranges,np.random.uniform())  # probabilistically select an index
 
+    def simulate(self, limit):
+        states=self.emitters
+        p_ranges=[self.starting_p[s.id] for s in states]
+        ix=self.random_pick(p_ranges)
+        node=states[ix]
+        emitted_seq=[]
+        for i in range(limit):
+            if node is None:
+                return emitted_seq
+            # p_ranges=[p for p,c in node.children.values()]
+
+
+# parents.get(i) returns (weight, parent) -> parents = {parent_id:(weight,parent_Node)}
+
+# if direction == 0: save both parents and children, if direction == 1: save only children, if -1 save only parents
 # initial probabilities, transition matrix, emission matrix, observation and state space,
 """states = ('Healthy', 'Fever')
 end_state = 'E'
--- ml/decoders.py
+++ ml/decoders.py
@@ -1,40 +1,50 @@
 import numpy as np
-from scipy.sparse import lil, csr
+import operator
+import copy
 
 
 def viterbi(observed_seq, hmm):
     """ recover a sequence of hidden states from a sequence of observations
 
-    :param hmm: container for initial probabilities, transition matrix, emission matrix, observation, and state space
+    :param hmm: container for initial probabilities, transition graph, emission matrix, observation, and state space
     :param observed_seq: a sequence of observations
-    :return: the most likely sequence of states
+    :return: the probability alongside with the most likely sequence of states
     """
-    if hmm is None or observed_seq is None or not hmm.is_valid():
+    if hmm is None or observed_seq is None or not hmm.is_valid() or len(observed_seq) == 0:
         return None
-    if len(observed_seq) == 0:
-        return []
 
     class TrellisNode:  # to keep only the survivor paths in the trellis diagram
-        def __init__(self, state_id, parent):
+        def __init__(self, state_id, node_parent):
             self.state_id = state_id
-            self.parent = parent  # keep a reference to parent
+            self.parent = node_parent  # keep a reference to parent
 
-    hidden_seq = []
-    states = hmm.states[2:]  # all states except the states B & E
-    emissions = hmm.emission_log_probs
-    transitions = hmm.transition_log_probs
-    nb_state = len(states)
-    state_window = [TrellisNode(i, None) for i in range(nb_state)]
-    posteriors = [hmm.starting_log_probs[i] + emissions[i][observed_seq[0]] for i in range(nb_state)]  # current scores
+    hidden_seq = np.zeros(len(observed_seq))
+    # a list of states except the beginning and ending states
+    states = hmm.emitters
+    # state window on the trellis diagram, width = 1, height = # states, slide at each observation step
+    state_window = [TrellisNode(s.id, None) for s in states]  # parent: None
+    # initialize scores
+    scores = [hmm.starting_p[s.id] + s.emission_p[observed_seq[0]] for s in states]  # updated at each observation step
+    for obs in observed_seq[1:]:  # construct survivor paths for each state
+        prev_states = state_window
+        prev_scores = copy.deepcopy(scores)  # scores = posterior log probabilities
+        state_window = [TrellisNode(s.id, None) for s in states]  # window for the next step
+        for i, s in enumerate(states):
+            parent_score = 0
+            for j, sc in enumerate(prev_scores):  # pick the best parent + transition score
+                # default transition_p = 0, parent = None
+                transition_p, parent = s.parents.get(prev_states[j].id, (0, None))
+                score = sc + transition_p
+                if parent_score < score:  # if there are two parents with the same score, choose the first one
+                    parent_score = score
+                    state_window[i].parent = prev_states[j]
+            scores[i] += s.emission_p[obs] + parent_score  # update scores
+    # end of observations, scores are calculated for len(states) surviving paths
+    # now pick the best score and backtrack the best survived path
+    index, max_score = max(enumerate(scores), key=operator.itemgetter(1))
+    node = state_window[index]
+    for i in range(len(hidden_seq) - 1, -1, -1):
+        hidden_seq[i] = node.id
+        node = node.parent
 
-    for obs in observed_seq[1:]:
-        prev_states = state_window  # allow the garbage collector to free the dead paths
-        state_window = [TrellisNode(i, None) for i in range(nb_state)]
-
-        for i,state in enumerate(transitions):
-            best_parent=0
-            for p in state.parents:
-                # if [p.id]
-                pass
-
-    return hidden_seq
+    return score, hidden_seq
